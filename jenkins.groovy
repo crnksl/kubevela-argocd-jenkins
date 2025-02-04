6712ff689fd22ceebe4081c04cc41002de90f8d0
crnksl <99171149+crnksl@users.noreply.github.com>
2025-02-04 14:38:29 +0300

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,35 +1,75 @@
 pipeline {
-    agent any
     environment {
         DOCKER_IMAGE = "crnksldockerhub/ci-app-example:latest"
     }
+    agent {
+        kubernetes {
+            cloud 'kubernetes'
+            yaml """
+            apiVersion: v1
+            kind: Pod
+            metadata:
+              labels:
+                app: jenkins-agent
+            spec:
+              containers:
+              - name: docker
+                image: docker:20.10.7
+                command:
+                - cat
+                tty: true
+                volumeMounts:
+                - mountPath: /var/run/docker.sock
+                  name: docker-socket
+              volumes:
+              - name: docker-socket
+                hostPath:
+                  path: /var/run/docker.sock
+            """
+        }
+    }
     stages {
-        stage('Clone Code') {
+        stage('Clone Repository') {
             steps {
-                git 'https://github.com/crnksl/kubevela-argocd-jenkins.git'
+                checkout([$class: 'GitSCM',
+                    branches: [[name: '*/main']],
+                    userRemoteConfigs: [[
+                        url: 'https://github.com/crnksl/kubevela-argocd-jenkins.git',
+                        credentialsId: 'github-credentials'
+                    ]]
+                ])
             }
         }
+
         stage('Build Docker Image') {
             steps {
-                script {
-                    sh 'docker build -t $DOCKER_IMAGE:$BUILD_NUMBER .'
+                container('docker') {
+                    sh '''
+                    docker build -t $DOCKER_IMAGE:$BUILD_NUMBER .
+                    docker push $DOCKER_IMAGE:$BUILD_NUMBER
+                    '''
                 }
             }
         }
-        stage('Push Docker Image') {
-            steps {
-                script {
-                    withDockerRegistry([credentialsId: 'dockerhub-credentials', url: '']) {
-                        sh 'docker push $DOCKER_IMAGE:$BUILD_NUMBER'
-                    }
-                }
-            }
-        }
+
         stage('Deploy to KubeVela') {
             steps {
-                sh '''
-                vela up -f ./vela.yaml
-                '''
+                withCredentials([file(credentialsId: 'kubeconfig', variable: 'KUBECONFIG')]) {
+                    sh '''
+                    export KUBECONFIG=$KUBECONFIG
+                    vela up -f ./vela.yaml
+                    '''
+                }
+            }
+        }
+
+        stage('Deploy to ArgoCD') {
+            steps {
+                withCredentials([file(credentialsId: 'kubeconfig', variable: 'KUBECONFIG')]) {
+                    sh '''
+                    argocd app sync nginx-jenkins-webapp
+                    '''
+                }
             }
         }
     }
